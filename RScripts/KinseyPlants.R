469155d394db2d4dbf804d160de6dabeeef61b73
justinmathias <justin.m.mathias@gmail.com>
2024-05-01 16:00:40 -0400

    extract elevation using WV DEM

--- RScripts/KinseyPlants.R
+++ RScripts/KinseyPlants.R
@@ -1,6 +1,6 @@
 #Created by Justin Mathias 4/30/2024 for Kinsey Reed
 library(easypackages)
-libraries(c("tidyverse","openxlsx"))
+libraries(c("tidyverse","openxlsx","elevatr","terra","sf"))
 
 dat <- read.xlsx("/Users/justinmathias/Library/CloudStorage/Dropbox/WVU Faculty/Research/Collaborating On/KinseyPlants/Data/all_data.xlsx")
 
@@ -31,3 +31,15 @@ sep.coords <- function(dat, in_col, return = "Coordinates") {
 #Separate coordinates into lat/lon
 Dat <- dat %>% 
   sep.coords(Coordinates)
+
+#Load in WV digital elevation model
+wv.elv <- rast("/Users/justinmathias/Library/CloudStorage/Dropbox/WVU Faculty/Research/Collaborating On/KinseyPlants/Data/wv_ned_02/wv_ned_02/prj.adf")
+wv.elv
+WV.elv <- project(wv.elv, "+proj=longlat +datum=WGS84 +no_defs")
+
+#Extract elevation for ClimateNA
+Dat1 <- Dat %>% 
+  mutate(extract(WV.elv, data.frame(Dat$Lon, Dat$Lat))) %>% 
+  select(-ID) %>% 
+  rename(Elv_m = wv_ned_02)
+Dat1
